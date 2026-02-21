9dd05fed5dea75d646a1a6ab99dc5babb1830565
arderh1x <whereyoursoul@gmail.com>
2026-02-21 12:41:27 +0200

    changed some modes in settings for GoodHost

--- GoodHost-3000/server.js
+++ GoodHost-3000/server.js
@@ -21,10 +21,18 @@ if (modeIndex !== -1 && args[modeIndex + 1]) mode = args[modeIndex + 1];
 console.log(`[System] Starting ${config.appName} v.${version}...`);
 
 
+app.use(cors());
+
+app.use((req, res, next) => {
+    res.setHeader("Content-Security-Policy", "default-src 'self'; img-src *; style-src *; " +
+        "script-src 'self' http://localhost:4000 http://localhost:6001; connect-src 'self' http://localhost:4000");
+    next();
+});
+
 switch (mode) {
-    case "mode1":
-        app.use(cors());
-        break;
+    case "mode1": break;
+    case "csp-balanced-fetch": break;
+    case "mode-insecure": break;
 
     case "csp-strict":
         app.use((req, res, next) => {
@@ -40,14 +48,6 @@ switch (mode) {
             next();
         });
         break;
-
-    case "csp-balanced-fetch":
-        app.use((req, res, next) => {
-            res.setHeader("Content-Security-Policy", "default-src 'self'; img-src *; style-src *; " +
-                "script-src 'self' http://localhost:4000 http://localhost:6001; connect-src 'self' http://localhost:4000");
-            next();
-        });
-        break;
 }
 
 
@@ -57,16 +57,16 @@ app.get("/emails", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-    //if (mode === "mode-insecure") res.sendFile(path.join(__dirname, "index.html"));
-    let html = fs.readFileSync(path.join(__dirname, "index.html"), "utf8");
     if (mode === "mode-sri-active") {
+        let html = fs.readFileSync(path.join(__dirname, "index.html"), "utf8");
         html = html.replace(`<script src="http://localhost:6001/react-mock.js"`,
             `<script src="http://localhost:6001/react-mock.js"
              integrity="sha256-1c47stpx27K9A9z7HBSs6KL2Q80XXUj5yxk5QTeFyKU="
              crossorigin="anonymous"`
         );
+        res.send(html);
     }
-    res.send(html);
+    else res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/main.js", (req, res) => {

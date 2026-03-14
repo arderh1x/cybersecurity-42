f078356bf3e14fca5db74d45a82e348565192b9f
arderh1x <whereyoursoul@gmail.com>
2026-03-15 01:54:49 +0200

    completed labwork8 - updated server 3000 to HTTPS

--- Proxy-8080/server.js
+++ Proxy-8080/server.js
@@ -3,6 +3,7 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 import proxy from "express-http-proxy";
+import https from "https";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -18,9 +19,13 @@ const modeIndex = args.indexOf("--mode");
 if (modeIndex !== -1 && args[modeIndex + 1]) mode = args[modeIndex + 1];
 console.log(`[System] Starting ${config.appName} v.${config.version}...`);
 
+const CA_BUNDLE = [fs.readFileSync("https-certificates/cert.pem")]
+const httpsAgent = new https.Agent({ ca: CA_BUNDLE });
 
-app.use("/", proxy("http://localhost:3000", {
+app.use("/", proxy("https://localhost:3443", {
     proxyReqOptDecorator: (proxyReqOpts, srcReq) => { // proxyReqOpts - request options which will send to 3000, srcReq - client's info
+        proxyReqOpts.agent = httpsAgent;
+        //proxyReqOpts.headers["x-proxy"] = "true";
         if (mode === "breach") console.log(srcReq.headers.cookie); // I forgot.
         return proxyReqOpts;
     }
